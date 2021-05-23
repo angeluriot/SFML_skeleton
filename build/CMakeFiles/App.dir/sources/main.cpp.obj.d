3705de1e5d5ea6581d37fb34238992f12964f27b
D1MENSI0N <angel.uriot@gmail.com>
2021-05-23 03:46:38 +0200

    Init

---
+++ build/CMakeFiles/App.dir/sources/main.cpp.obj.d
@@ -0,0 +1,231 @@
+CMakeFiles/App.dir/sources/main.cpp.obj: \
+ D:\Bureau\Development\Projects\-\ Templates\SFML_skeleton\sources\main.cpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Config.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Clock.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Export.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Time.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Err.hpp \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ostream \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ios \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\iosfwd \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\c++config.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\os_defines.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\cpu_defines.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\pstl\pstl_config.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stringfwd.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\memoryfwd.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\postypes.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cwchar \
+ c:\mingw\include\wchar.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h c:\mingw\include\features.h \
+ c:\mingw\include\wctype.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
+ c:\mingw\include\sys\types.h c:\mingw\include\stdio.h \
+ c:\mingw\include\sys/types.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\stdarg.h \
+ c:\mingw\include\stdlib.h c:\mingw\include\direct.h \
+ c:\mingw\include\sys/stat.h c:\mingw\include\conio.h \
+ c:\mingw\include\io.h c:\mingw\include\stdint.h c:\mingw\include\time.h \
+ c:\mingw\include\locale.h c:\mingw\include\process.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\exception \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\exception.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\exception_ptr.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\exception_defines.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\cxxabi_init_exception.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\typeinfo \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\hash_bytes.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\new \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\nested_exception.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\move.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\concept_check.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\type_traits \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\char_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_algobase.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\functexcept.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\cpp_type_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\type_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\numeric_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_pair.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_iterator_base_types.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_iterator_base_funcs.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\debug\assertions.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_iterator.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\ptr_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\debug\debug.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\predefined_ops.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cstdint \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\stdint.h \
+ c:\mingw\include\stdint.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\localefwd.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\c++locale.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\clocale \
+ c:\mingw\include\locale.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cctype \
+ c:\mingw\include\ctype.h c:\mingw\include\wctype.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\ios_base.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\atomicity.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\gthr.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\gthr-default.h \
+ c:\mingw\include\errno.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\atomic_word.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_classes.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\string \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\allocator.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\c++allocator.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\new_allocator.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\ostream_insert.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\cxxabi_forced.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_function.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\backward\binders.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\range_access.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\initializer_list \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\basic_string.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\alloc_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\alloc_traits.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\string_view \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\limits \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\functional_hash.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\string_view.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\string_conversions.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cstdlib \
+ c:\mingw\include\stdlib.h c:\mingw\include\errno.h \
+ c:\mingw\include\alloca.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\std_abs.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cstdio \
+ c:\mingw\include\stdio.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cerrno \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\basic_string.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_classes.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\system_error \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\error_constants.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\stdexcept \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\streambuf \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\streambuf.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\basic_ios.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_facets.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\cwctype \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\ctype_base.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\streambuf_iterator.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\ctype_inline.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_facets.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\basic_ios.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\ostream.tcc \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/FileInputStream.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/InputStream.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/NonCopyable.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Lock.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/MemoryInputStream.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Mutex.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Sleep.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/String.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Utf.hpp \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\algorithm \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\utility \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_relops.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_algo.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\algorithmfwd.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_heap.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_tempbuf.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_construct.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\uniform_int_dist.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\pstl\glue_algorithm_defs.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\functional \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\tuple \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\array \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\uses_allocator.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\invoke.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\refwrap.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\std_function.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\unordered_map \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ext\aligned_buffer.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\hashtable.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\hashtable_policy.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\node_handle.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\optional \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\enable_special_members.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\unordered_map.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\erase_if.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\vector \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_uninitialized.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_vector.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_bvector.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\vector.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\pstl\execution_defs.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\locale \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_facets_nonio.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\ctime c:\mingw\include\time.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\time_members.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\mingw32\bits\messages_members.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\codecvt.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_facets_nonio.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\locale_conv.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\unique_ptr.h \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Utf.inl \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\iterator \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\istream \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\istream.tcc \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stream_iterator.h \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/String.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Thread.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Thread.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/ThreadLocal.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/ThreadLocalPtr.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/ThreadLocalPtr.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Vector2.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Vector2.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Vector3.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/System/Vector3.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Clipboard.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Export.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Context.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/GlResource.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/ContextSettings.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Cursor.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Event.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Joystick.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Keyboard.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Mouse.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Sensor.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Touch.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/VideoMode.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/Window.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/WindowHandle.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Window/WindowStyle.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/BlendMode.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Export.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/CircleShape.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Shape.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Drawable.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/RenderStates.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Transform.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Rect.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Rect.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Transformable.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/VertexArray.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Vertex.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Color.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/PrimitiveType.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/ConvexShape.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Font.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Glyph.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Texture.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Image.hpp \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\map \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_tree.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_map.h \
+ c:\mingw\lib\gcc\mingw32\9.2.0\include\c++\bits\stl_multimap.h \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/RectangleShape.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/RenderTarget.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/View.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/RenderTexture.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/RenderWindow.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Shader.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Glsl.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Glsl.inl \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Sprite.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/Text.hpp \
+ D:/Bureau/Development/Projects/-\ Templates/SFML_skeleton/libraries/SFML/MinGW/x86/include/SFML/Graphics/VertexBuffer.hpp
